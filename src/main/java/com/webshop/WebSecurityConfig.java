98473174787f94d7cbe5a73697e6934b2a22310c
Paweł Cesar Sanjuan Szklarz <p@eiipii.com>
2018-10-22 22:47:21 +0200

    use autoconfig for web

--- src/main/java/com/webshop/WebSecurityConfig.java
+++ src/main/java/com/webshop/WebSecurityConfig.java
@@ -1,54 +1,55 @@
 package com.webshop;
 
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.UUID;
 
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    private final UUID adminRandomSessionToken;
-//
-//    public WebSecurityConfig() {
-//        adminRandomSessionToken = UUID.randomUUID();
-//        System.err.print(adminRandomSessionToken.toString());
-//        //TODO create proper logging. minor priority.
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .cors().disable()
-//                .authorizeRequests()
-//                .antMatchers("/api/session", "/api/categories", "/api/categories/**", "/api/products", "/api/products/**",
-//                        "/api/pictures", "/api/pictures/**").anonymous()
-//                .anyRequest().authenticated()
-//                .and().httpBasic();
-//        //TODO configure basic auth
-//    }
-//
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        //TODO change withDefaultPasswordEncoder minor priority.
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("admin")
-//                        .password(adminRandomSessionToken.toString())
-//                        .roles("USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
-//}
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UUID adminRandomSessionToken;
+
+    public WebSecurityConfig() {
+        adminRandomSessionToken = UUID.randomUUID();
+        System.err.print(adminRandomSessionToken.toString());
+        //TODO create proper logging. minor priority.
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().disable()
+                .authorizeRequests()
+                .antMatchers("/api/session", "/api/categories", "/api/categories/**", "/api/products", "/api/products/**",
+                        "/api/pictures", "/api/pictures/**").anonymous()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+        //TODO configure basic auth
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        //TODO change withDefaultPasswordEncoder minor priority.
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password(adminRandomSessionToken.toString())
+                        .password("admintest")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/com/webshop/controllers/ProductController.java
+++ src/main/java/com/webshop/controllers/ProductController.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-
 public class ProductController extends AbstractControllerExceptionHandler {
 
     @Autowired
